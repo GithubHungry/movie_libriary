d8b777e349b2c962ef9e098ddbddeb15a7942fc3
GithubHungry <bardiervadim97@gmail.com>
2020-07-29 17:25:10 +0300

    add_models_template_simple_view

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin.site.register(models.Category)
+admin.site.register(models.Genre)
+admin.site.register(models.Movie)
+admin.site.register(models.MovieShots)
+admin.site.register(models.Actor)
+admin.site.register(models.Rating)
+admin.site.register(models.RatingStar)
+admin.site.register(models.Reviews)
