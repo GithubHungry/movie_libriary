dc0812a9735a88cea5f3f97edb9d899f1868792d
GithubHungry <bardiervadim97@gmail.com>
2020-07-31 11:51:48 +0300

    add_rating

--- movies/urls.py
+++ movies/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.MovieList.as_view(), name='movie_list'),
     path('filter/', views.FilterMoviesView.as_view(), name='filter'),
+    path("add-rating/", views.AddStarRating.as_view(), name='add_rating'),
     path('json-filter/', views.JsonFilterMoviesView.as_view(), name='json_filter'),
     path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'),
     path('actors/<str:slug>/', views.ActorView.as_view(), name='actor_detail'),
