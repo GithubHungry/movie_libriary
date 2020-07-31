3f2c47598c2e29bab74862f7e9f344a5ac76eed2
GithubHungry <bardiervadim97@gmail.com>
2020-07-31 10:43:41 +0300

    add_json_filter

--- movies/urls.py
+++ movies/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.MovieList.as_view(), name='movie_list'),
     path('filter/', views.FilterMoviesView.as_view(), name='filter'),
+    path('json-filter/', views.JsonFilterMoviesView.as_view(), name='json_filter'),
     path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'),
     path('actors/<str:slug>/', views.ActorView.as_view(), name='actor_detail'),
     path('review/<int:pk>/', views.AddReview.as_view(), name='add_review'),
